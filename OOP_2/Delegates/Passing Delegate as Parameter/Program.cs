837022912d9c9bd242e0f3831bed12919595e75a
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-08-31 15:12:26 +0400

    Add files via upload

---
+++ OOP_2/Delegates/Passing Delegate as Parameter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Passing_Delegate_as_Parameter
+{
+    internal class Program
+    {
+        //passing methods as arguments
+        static void Main(string[] args)
+        {
+            MathOperation operation = new MathOperation(Add);
+            operation +=Multiply;
+            Calculator(5,2,Add);
+            Calculator(5,2,Multiply);
+            Calculator(5,2,(a,b)=> a-b); //usable with lambda expressions
+            Console.ReadLine();
+        }
+        public static void Calculator(int a, int b, MathOperation op)
+        {
+            int result = op(a, b);
+            Console.WriteLine($"Result: {result}");
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+    delegate int MathOperation(int x, int y);
+}
