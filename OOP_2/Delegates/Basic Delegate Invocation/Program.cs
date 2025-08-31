e48f03890e049018bba675956e8d3af1ec6f4319
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-08-31 14:55:20 +0400

    Add files via upload

---
+++ OOP_2/Delegates/Basic Delegate Invocation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Basic_Delegate_Invocation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation add = new MathOperation(Add);
+            MathOperation multiply = new MathOperation(Multiply);
+            int x = add.Invoke(2,5);
+            int y = multiply.Invoke(2,5);
+            Console.WriteLine("Values with same delegate but different methods.");
+            Console.WriteLine($"With method Add: {x}");
+            Console.WriteLine($"With method Multiply: {y}");
+            Console.ReadLine();
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+    delegate int MathOperation(int x, int y);
+}
