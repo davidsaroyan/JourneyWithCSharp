f05899d2356e0896e75a70b791642a13d65fea08
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-08-31 08:38:03 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+    class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    class CarDTO
+    {
+        public int Id { get; set; }
+        public string ShortDesc { get; set; }
+    }
+    static class Mapper<TSource, TDestination> 
+    {
+        static TDestination Map(TSource source,Func<TSource,TDestination> func) 
+        {
+
+        }
+
+    }
+}
