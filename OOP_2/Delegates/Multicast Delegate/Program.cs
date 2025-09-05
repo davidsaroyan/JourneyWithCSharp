d6e074426ebfaa0bc839c4acbc3d0635070a6582
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-09-05 09:52:27 +0400

    Add files via upload

---
+++ OOP_2/Delegates/Multicast Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Multicast_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer(PrintUpperCase);
+            p += PrintLowerCase;
+            p.Invoke("Same message with different methods");
+            Console.ReadLine();
+        }
+        public static void PrintUpperCase(string msg)
+        {
+            Console.WriteLine(msg.ToUpper());
+        }
+        public static void PrintLowerCase(string msg)
+        {
+            Console.WriteLine(msg.ToLower());
+        }
+    }
+    delegate void Printer(string msg);
+}
