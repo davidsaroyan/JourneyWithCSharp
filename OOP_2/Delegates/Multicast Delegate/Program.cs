9c83ac4d482ea57dba599a553ebfe2c84f3712f8
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-09-05 10:00:39 +0400

    Add files via upload

---
+++ OOP_2/Delegates/Multicast Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Multicast_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer(PrintUpperCase);
+            p += PrintLowerCase;
+            p.Invoke("Same message with different methods");
+            Console.WriteLine("\nNow lets use GetInvocationList\n");
+            var methodList = p.GetInvocationList();
+            foreach (var method in methodList) 
+            {
+                method.DynamicInvoke("Message with Dynamic Invoke");
+            }
+            Console.ReadLine();
+        }
+        public static void PrintUpperCase(string msg)
+        {
+            Console.WriteLine(msg.ToUpper());
+        }
+        public static void PrintLowerCase(string msg)
+        {
+            Console.WriteLine(msg.ToLower());
+        }
+    }
+    delegate void Printer(string msg);
+}
