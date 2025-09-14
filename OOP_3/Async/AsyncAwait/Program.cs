9fbd4d9ad9fe58fbe958020beb738058c98066ba
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-09-14 18:31:44 +0400

    Add files via upload

---
+++ OOP_3/Async/AsyncAwait/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await PrintAsync();
+            Console.WriteLine("Main things");
+            Console.ReadLine();
+        }
+        static void Print()
+        {
+            Thread.Sleep(3000); // testing sleep
+            Console.WriteLine("Async Hello");
+        }
+        static async Task PrintAsync()
+        {
+            Console.WriteLine("Start of async method");
+            await Task.Run(Print);
+            Console.WriteLine("End of PrintAsync");
+        }
+    }
+}
