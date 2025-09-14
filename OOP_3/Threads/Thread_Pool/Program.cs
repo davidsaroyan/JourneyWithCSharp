075ba8530f2a1cb467f271014f15eca6cd436211
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-09-14 19:34:58 +0400

    Add files via upload

---
+++ OOP_3/Threads/Thread_Pool/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Thread_Pool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(DoWork);
+            Thread thread2 = new Thread(DoWork);
+
+            thread1.Start("Thread 1");
+            thread2.Start("Thread 2");
+
+            ThreadPool.QueueUserWorkItem(DoWork, "Threadpool 1");
+            ThreadPool.QueueUserWorkItem(DoWork, "Threadpool 2");
+
+            Console.WriteLine("Main thread going on...");
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("All Threads have finished their work.");
+            Console.ReadLine();
+        }
+        public static void DoWork(object threadName)
+        {
+            Console.WriteLine($"Thread '{threadName}' is Starting its work...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Thread '{threadName}' has finished its work!");
+        }
+    }
+}
