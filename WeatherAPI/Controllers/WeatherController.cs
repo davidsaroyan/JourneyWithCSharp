02fe0b459e2e5297bfa550556f810f2536e9c96e
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-09-28 18:56:49 +0400

    Add files via upload

---
+++ WeatherAPI/Controllers/WeatherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Services;
+namespace WeatherApi.Controllers { 
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class WeatherController : ControllerBase 
+    { 
+        private readonly WeatherService _weatherService;
+        public WeatherController(WeatherService weatherService) 
+        { _weatherService = weatherService; } 
+        [HttpGet("{city}")] 
+        public async Task<IActionResult> GetWeather(string city) 
+        { 
+            var weather = await _weatherService.GetWeatherAsync(city);
+            if (weather == null) return NotFound(new { message = "City not found" });
+            return Ok(new 
+            { 
+                City = weather.Name,
+                Temperature = weather.Main.Temp,
+                Condition = weather.Weather[0].Description, 
+                Wind = weather.Wind.Speed 
+            }
+            ); 
+        }
+        [HttpGet("save/{city}")]
+        public async Task<IActionResult> GetWeatherFilePath(string city)
+        {
+            var weather = await _weatherService.SaveWeatherToFileAsync(city);
+            if (weather == null) return NotFound(new { message = "City not found" });
+            return Ok(weather);
+        }
+    } 
+}
---
+++ WeatherAPI/Services/WeatherService.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+
+namespace WeatherAPI.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "d95d6f5ebad1bd7d71325806e4569d58";
+        private readonly string _folderPath;
+        
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _folderPath = Path.Combine(Directory.GetCurrentDirectory(), "WeatherData"); 
+            Directory.CreateDirectory(_folderPath);
+        }
+        public async Task<WeatherResponse> GetWeatherAsync(string city)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={_apiKey}&units=metric";
+
+            return await _httpClient.GetFromJsonAsync<WeatherResponse>(url);
+        }
+        public async Task<string> SaveWeatherToFileAsync(string city)
+        {
+            var weather = await this.GetWeatherAsync(city);
+            if (weather == null)
+                return null;
+
+            var json = JsonSerializer.Serialize(weather, new JsonSerializerOptions { WriteIndented = true });
+            var filePath = Path.Combine(_folderPath, $"{city}_{DateTime.UtcNow:yyyyMMdd_HHmmss}.json");
+
+            await File.WriteAllTextAsync(filePath, json);
+
+            return filePath;
+        }
+    }
+    public class WeatherRequest 
+    {
+        public string City { get; set; }
+    }
+    public class WeatherResponse 
+    {
+        public MainInfo Main { get; set; }
+        public WindInfo Wind { get; set; }
+        public WeatherInfo[] Weather { get; set; }    
+        public string Name { get; set; }
+    }
+    public class WeatherInfo
+    {
+        public string Description { get; set; }
+    }
+    public class MainInfo 
+    {
+        public double Temp { get; set; } 
+    }
+    public class WindInfo 
+    {
+        public double Speed { get; set; }
+    }
+}
