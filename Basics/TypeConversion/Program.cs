73e0a85c2548376089c1bdbc4d8675eb0f50810f
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-03 18:34:30 +0400

    Add files via upload

---
+++ Basics/TypeConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number");
+            string input = Console.ReadLine();
+            int iNum = Convert.ToInt32(input);
+            Console.WriteLine("Enter an float number");
+            input = Console.ReadLine();
+            float fNum = Convert.ToSingle(input);
+            Console.WriteLine("Enter an double number");
+            input = Console.ReadLine();
+            double dNum = Convert.ToDouble(input);
+            Console.WriteLine($"Int: {iNum}");
+            Console.WriteLine($"Float: {fNum}");
+            Console.WriteLine($"Double: {dNum}");
+            Console.ReadLine();
+        }
+    }
+}
