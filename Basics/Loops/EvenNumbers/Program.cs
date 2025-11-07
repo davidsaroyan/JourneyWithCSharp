67554802fe945d59f05bc99db107b773b2742e95
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-07 18:58:16 +0400

    Add files via upload

---
+++ Basics/Loops/EvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0) Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Evens until your number");
+            Console.ReadLine();
+        }
+    }
+}
