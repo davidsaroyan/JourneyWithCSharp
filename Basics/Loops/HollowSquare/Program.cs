29e0b404dce48bf6750b2a644ec34a7debf639b4
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-07 19:05:28 +0400

    Add files via upload

---
+++ Basics/Loops/HollowSquare/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HollowSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (i == 1 || i == n || j == 1 || j == n)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
