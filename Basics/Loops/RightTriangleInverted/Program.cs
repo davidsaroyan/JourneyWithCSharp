1084872148dd4eec7b766f71158600b4da1b0d3b
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-07 19:10:04 +0400

    Add files via upload

---
+++ Basics/Loops/RightTriangleInverted/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RightTriangleInverted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
