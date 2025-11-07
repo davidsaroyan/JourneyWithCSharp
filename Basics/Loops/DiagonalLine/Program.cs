a677f2730944f5ad201664b6b03db85741fa0f18
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-07 19:06:02 +0400

    Add files via upload

---
+++ Basics/Loops/DiagonalLine/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DiagonalLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        Console.Write("*");
+                    else
+                        Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
