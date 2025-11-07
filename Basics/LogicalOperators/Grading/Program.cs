617a203edf76f936a2e72f9622f5f1010f1bba2b
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-07 19:46:17 +0400

    Add files via upload

---
+++ Basics/LogicalOperators/Grading/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Grading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Grade(0-100)");
+            int grade = int.Parse(Console.ReadLine());
+            string result = ""; 
+            Console.WriteLine();
+            if (grade >= 0 && grade < 40) result = "Bad";
+            else if (grade >= 40 && grade < 60) result = "Average";
+            else if (grade >= 60 && grade < 90) result = "Good";
+            else if (grade >= 90 && grade <= 100) result = "Excellent";
+            else result = "Dumb";
+            Console.WriteLine($"You are {result} student :)");
+        }
+    }
+}
