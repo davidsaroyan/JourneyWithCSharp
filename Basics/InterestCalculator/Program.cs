c500474d2b85d7743e28015c228f1f869f0af455
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-10 00:05:24 +0400

    Add files via upload

---
+++ Basics/InterestCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace InterestCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Starting amount of money, Rate, and time (years)");
+            double start = double.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine());
+            double time = double.Parse(Console.ReadLine());
+            double simpleInterest = (start * rate * time) / 100;
+            double afterCI = start * Math.Pow((1 + rate / 100), time);
+            double compoundInterest = afterCI - start;
+            Console.WriteLine($"You will earn (simple interest): {simpleInterest:F3}$");
+            Console.WriteLine($"You will earn (compound interest): {compoundInterest:F3}$");
+            Console.ReadLine();
+        }
+    }
+}
