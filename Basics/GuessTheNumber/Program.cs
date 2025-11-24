cbc161004d2c3109093a57cd2a02c024c7dd5c91
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-24 12:26:16 +0400

    Add files via upload

---
+++ Basics/GuessTheNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int x = random.Next(1, 100);
+            Console.WriteLine("Guess the number from 1-100");
+            int guess = int.Parse(Console.ReadLine());
+            while (guess != x)
+            {
+                if (guess > x )
+                {
+                    Console.WriteLine($"{guess} is higher than my number");
+                    guess = int.Parse(Console.ReadLine());
+                }else if (guess < x) 
+                {
+                    Console.WriteLine($"{guess} is lower than my number");
+                    guess = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Congrats you guessed my number");
+        }
+    }
+}
