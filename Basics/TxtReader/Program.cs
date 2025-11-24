1f1bbbcefe3685cf75bca833b4ac5bc32d5931bb
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-24 18:29:50 +0400

    Add files via upload

---
+++ Basics/TxtReader/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TxtReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me path to your txt file");
+            string path = Console.ReadLine();
+            string text = File.ReadAllText(path);
+            Console.WriteLine("This is your text: \n");
+            Console.WriteLine(text);
+            var creationTime = File.GetCreationTime(path);
+            Console.WriteLine($"You made this in {creationTime}");
+        }
+    }
+}
