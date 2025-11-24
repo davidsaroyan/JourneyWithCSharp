bf43386e780e64c3b65da78134d812f032ae827e
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-11-24 12:42:11 +0400

    Add files via upload

---
+++ Basics/PolindromeChecker/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PolindromeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type anything i'll check if its polindrome");
+            string text = Console.ReadLine();
+            text = text.Replace(" ", "");
+            string reversed = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversed = reversed + text[i];
+            }
+            if (string.Equals(text, reversed, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Your text is polindrome :)");
+            }
+            else
+            {
+                Console.WriteLine("Your text is NOT polindrome :(");
+            }
+        }
+    }
+}
