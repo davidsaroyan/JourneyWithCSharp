dafd7c8318dc470bef2dbb31fb934e302ffaf964
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2025-12-27 12:43:52 -0800

    Add files via upload

---
+++ OOP_1/Classes/ZooSystem/Animals.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooSystem;
+
+namespace ZooSystem
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public abstract void MakeSound();
+        public virtual void Move()
+        {
+            Console.WriteLine($"{Name} moves in a generic way");
+        }
+        public void Info()
+        {
+            Console.WriteLine($"{Name} is general Animal");
+        }
+    }
+    class Dog : Animal,  IPet
+    {
+        public string Breed { get; set; }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+        public new void Info()
+        {
+            Console.WriteLine($"{Name} is a Dog of breed {Breed}");
+        }
+        public void play()
+        {
+            Console.WriteLine($"{Name} is playing happily.");
+        }
+    }
+    class Bulldog : Dog
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} walks slowly with heavy steps");
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} grumbles and says: Woof!");
+        }
+        public new void Info()
+        {
+            Console.WriteLine($"{Name} is tough Bulldog.");
+        }
+    }
+    class Cat : Animal,IPet
+    {
+        public bool IsIndoor { get; set; }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+        public new void Info()
+        {
+            Console.WriteLine($"{Name} is a {(IsIndoor ? "house" : "street")} cat");
+        }
+        public void play()
+        {
+            Console.WriteLine($"{Name} is playing happily.");
+        }
+    }
+    class Siamese : Cat
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} walks gracefully lika a dancer");
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} purrs softly.");
+        }
+        public new void Info()
+        {
+            Console.WriteLine($"{Name} is an elegant Siamese cat.");
+        }
+    }
+    class Bird : Animal , IFlyable
+    {
+        public bool CanFly { get; set; }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Tweet!");
+        }
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} {(CanFly ? "flies" : "walks")} around.");
+        }
+        public new void Info()
+        {
+            Console.WriteLine($"{Name} is a bird.");
+        }
+
+        public void Fly()
+        {
+            if (CanFly) 
+            {
+                Console.WriteLine($"{Name} flaps and flies");
+            }
+        }
+    }
+    class Parrot : Bird
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} mimics: Hello!");
+        }
+        public new void Info()
+        {
+            Console.WriteLine($"{Name} is a talking Parrot.");
+        }
+    }
+}
---
+++ OOP_1/Classes/ZooSystem/Program.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+
+namespace ZooSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo<Animal> zoo = new Zoo<Animal>();
+            Dog dog = new Dog() { Name = "Rex", Age = 2, Breed = "Shepherd" };
+            Bulldog bullDog = new Bulldog() { Name = "Cookie", Age = 3, Breed = "French Bulldog"};
+            Cat cat = new Cat() { Name= "Princces", Age = 4, IsIndoor = true};
+            Siamese siameseCat = new Siamese() { Name = "Ariel", Age = 1, IsIndoor = true };
+            Bird bird = new Bird() { Name = "Baldy", Age = 2,  CanFly = true };
+            Parrot parrot = new Parrot() { Name = "Perry", Age = 1, CanFly = true };
+            zoo.Add(dog);
+            zoo.Add(bullDog);
+            zoo.Add(cat);
+            zoo.Add(siameseCat);
+            zoo.Add(bird);
+            zoo.Add(parrot);
+            Console.WriteLine("-----AllSounds-------");
+            zoo.MakeAllSounds();
+            Console.WriteLine("-----AllMoves-------");
+            zoo.MoveAllAnimals();
+            Console.WriteLine("-----AllInfos-------");
+            zoo.ShowAnimalInfos();
+            Console.WriteLine("---Let pets play----");
+            zoo.PlayWithPets();
+            Console.WriteLine("---Let birds fly");
+            zoo.LetBirdsFly();
+            Console.WriteLine("------Polymorphism------");
+            for (int i = 0; i < zoo.Count; i++) 
+            {
+                if (zoo[i] is Bulldog a) 
+                {
+                    a.Info();
+                }
+                if (zoo[i] is Siamese b)
+                {
+                    b.Info();
+                }
+                if (zoo[i] is Parrot c)
+                {
+                    c.Info();
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+    public interface IPet
+    {
+        public abstract void play();
+    }
+    public interface IFlyable
+    {
+        public abstract void Fly();
+    }
+    public class Zoo<T> : IList<Animal> where T : Animal
+    {
+        #region IListImplementation    
+        public T this[int index]
+        {
+            get => (T)animals[index];
+            set => animals[index] = value;
+        }
+        private List<Animal> animals = new List<Animal>();
+        Animal IList<Animal>.this[int index] { get => animals[index]; set => animals[index] = value; }
+
+        public int Count => animals.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Clear()
+        {
+            animals.Clear();
+        }
+
+        public void CopyTo(Animal[] array, int arrayIndex)
+        {
+            animals.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return animals.GetEnumerator();
+        }
+
+        public void RemoveAt(int index)
+        {
+            animals.RemoveAt(index);
+        }
+        public void Add(Animal item)
+        {
+            animals.Add(item);
+        }
+        void ICollection<Animal>.Add(Animal item)
+        {
+            animals.Add(item);
+        }
+
+        bool ICollection<Animal>.Contains(Animal item)
+        {
+            return animals.Contains(item);
+        }
+
+        IEnumerator<Animal> IEnumerable<Animal>.GetEnumerator()
+        {
+            return animals.GetEnumerator();
+        }
+
+        int IList<Animal>.IndexOf(Animal item)
+        {
+            return animals.IndexOf(item);
+        }
+
+        void IList<Animal>.Insert(int index, Animal item)
+        {
+            animals.Insert(index, item);
+        }
+
+        bool ICollection<Animal>.Remove(Animal item)
+        {
+            return animals.Remove(item);
+        }
+        #endregion
+        #region ExtraMethods
+        public void MakeAllSounds()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                animals[i].MakeSound();
+            }
+        }
+        public void MoveAllAnimals()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                animals[i].Move();
+            }
+        }
+        public void ShowAnimalInfos()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                animals[i].Info();
+            }
+        }
+        public void PlayWithPets()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if(animals[i] is IPet a)
+                {
+                    a.play();
+                } 
+            }
+        }
+        public void LetBirdsFly()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i] is IFlyable a)
+                {
+                    a.Fly();
+                }
+            }
+        }
+        #endregion
+    }
+
+}
