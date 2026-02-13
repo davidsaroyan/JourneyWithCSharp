30f90d91eee3138e8582ecc52ffa6bacfac971c4
davidsaroyan <101281955+davidsaroyan@users.noreply.github.com>
2026-02-13 21:59:33 +0400

    Add files via upload

---
+++ OOP_1/Interfaces/CloneWars/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneWars
+{
+    internal class Person : ICloneable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address HomeAddress { get; set; }
+        public Person(int id, string name, Address homeAddress)
+        {
+            Id = id;
+            Name = name;
+            HomeAddress = homeAddress;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+        public Person DeepClone()
+        {
+            var clone = (Person)this.MemberwiseClone();
+            clone.HomeAddress = new Address(HomeAddress.City, HomeAddress.Street);
+            return clone;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Id: {Id}\nName: {Name}\nLocation: {HomeAddress.Street} street, {HomeAddress.City}\n");
+        }
+    }
+}
---
+++ OOP_1/Interfaces/CloneWars/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CloneWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var original = new Person(12345, "David", new Address("Yerevan", "Kirkorian"));
+            Console.WriteLine("Original:");
+            original.PrintInfo();
+
+            var shallow = (Person)original.Clone();
+            shallow.HomeAddress.City = "London";               // change clone , original affected
+
+            Console.WriteLine("After changing shallow clone's city, original now:");
+            original.PrintInfo();   // shows London — proof of shared reference
+
+            Console.WriteLine("Shallow clone itself:");
+            shallow.PrintInfo();
+
+            var deep = original.DeepClone();
+            deep.HomeAddress.City = "Paris";                   // change deep, original unchanged
+
+            Console.WriteLine("After changing deep clone's city, original still:");
+            original.PrintInfo();
+
+            Console.WriteLine("Deep clone itself:");
+            deep.PrintInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
